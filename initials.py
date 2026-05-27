e1ef2c7de673864cad8c36360ed8c96335271f7d
abmxy <2425402777@live.gctu.edu.gh>
2026-05-27 21:45:33 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials ABM in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Mark Asante-Boamah
+# STUDENT ID:  2425402777
+# Email: 2425402777@live.gctu.edu.gh
+# Grader: Carolyn
+#
+# On my honor,Mark Asante-Boamah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Prints my initials (ABM) in 12x10 large formats, strictly 60 characters wide per line.
+    print()
+    print("...ABM")
+    print()
+    print(".......AAAA............BBBBBBBBBB..........MMMM....MMMM.....")
+    print("......AA..AA...........BB.......BB.........MMMM....MMMM.....")
+    print(".....AA....AA..........BB.......BB.........MM.MM..MM.MM.....")
+    print("....AA......AA.........BB.......BB.........MM.MM..MM.MM.....")
+    print("....AAAAAAAAAA.........BBBBBBBBBB..........MM..MMMM..MM.....")
+    print("....AAAAAAAAAA.........BBBBBBBBBB..........MM..MMMM..MM.....")
+    print("....AA......AA.........BB.......BB.........MM...MM...MM.....")
+    print("....AA......AA.........BB.......BB.........MM...MM...MM.....")
+    print("....AA......AA....**...BB.......BB....**...MM........MM...**")
+    print("....AA......AA....**...BBBBBBBBBB.....**...MM........MM...**")
+    print()
 
 
 main()
